f0cae84741783e8ee56d6b7fe75c860be3a9f453
alexgodyl <alexey.godyl@ids-group.co.uk>
2023-02-20 16:35:28 +0300

    Add project files.

---
+++ FreeSpireDocExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spire.Doc;
+
+public class Program
+{
+    public static void Main()
+    {
+        var document = new Document();
+
+        string docPath = @"C:\example";
+        document.LoadFromFile(Path.Combine(docPath, "example.docx"));
+        document.SaveToFile(Path.Combine(docPath, "example.pdf"), FileFormat.PDF);
+    }
+}
